855c0af1709d93bcf771cc4c2ca185c24d305239
Kautsarhidayat <kautsar.i.hidayat@gmail.com>
2024-03-23 09:00:16 +0700

    '

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    Public function index()
+    {
+        echo"<h1>perkenalkan</h1>";
+        echo"Nama saya Kautsar Imam Hidayat
+            Saya tinggal di bekasi
+            Olahraga yang disukai adalah 
+            Basket";
+    }
+}
